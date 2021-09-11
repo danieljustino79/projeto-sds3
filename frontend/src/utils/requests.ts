82cc179f86ade5fb4756c772664516e3c5eceef5
daniel justino <danieljustino79@gmail.com>
2021-09-11 11:35:37 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
-export const BASE_U = 'http://localhost:8080';
-export const BASE_URL = 'https://sds3-dantino.herokuapp.com';
+export const BASE_U = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-dantino.herokuapp.com';
 
